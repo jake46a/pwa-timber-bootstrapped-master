a847e6dbb66d8b3463466911cb25c1bc00326cce
Unknown <jake@uberingdenver.com>
2018-04-13 22:18:44 -0600

    commrent systam still -but got new comment_form() to work with args in Timber:)

--- single.php
+++ single.php
@@ -8,11 +8,39 @@
  * @subpackage  Timber
  * @since    Timber 0.1
  */
+$pid=get_the_ID();
+ob_start();
+$args = array(
+     'id_form'           => 'commentform',  // that's the wordpress default value! delete it or edit it ;)
+        'id_submit'         => 'commentsubmit',
+        'title_reply'       => __( 'Leave a Reply', 'wp-bootstrap-starter' ),  // that's the wordpress default value! delete it or edit it ;)
+        'title_reply_to'    => __( 'Leave a Reply to %s', 'wp-bootstrap-starter' ),  // that's the wordpress default value! delete it or edit it ;)
+        'cancel_reply_link' => __( 'Cancel Reply', 'wp-bootstrap-starter' ),  // that's the wordpress default value! delete it or edit it ;)
+        'label_submit'      => __( 'Post Comment', 'wp-bootstrap-starter' ),  // that's the wordpress default value! delete it or edit it ;)
+        'comment_field' =>  '<p><textarea placeholder="Start typing..." id="comment" class="form-control" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>',
+        'comment_notes_after' => '<p class="form-allowed-tags">' . __( 'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes:', 'wp-bootstrap-starter' ) . '</p><div class="alert alert-info">' . allowed_tags() . '</div>'
 
+        // So, that was the needed stuff to have bootstrap basic styles for the form elements and buttons
+
+        // Basically you can edit everything here!
+        // Checkout the docs for more: http://codex.wordpress.org/Function_Reference/comment_form
+        // Another note: some classes are added in the bootstrap-wp.js - ckeck from line 1
+
+    );
+
+comment_form($args);
+$form = ob_get_clean();
+function cmf(){
+    global $form;
+    return $form;
+    }
+    
+$content['form'] = $form;
 $context = Timber::get_context();
 $post = Timber::query_post();
 $context['post'] = $post;
 
+//echo $form;
 if ( post_password_required( $post->ID ) ) {
 	Timber::render( 'single-password.twig', $context );
 } else {
